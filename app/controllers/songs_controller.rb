d5c74248c94cbf7816ddece9403f9451ec686e04
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-11-23 18:24:53 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -42,6 +42,12 @@ class SongsController < ApplicationController
    @songs.artist = params[:artist]
    @songs.save
    
+       @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.genre_ids = params[:genres]
+    @song.save
+   
     flash[:message] = "Successfully created song."
   redirect to "/songs/#{@songs.slug}"
 
